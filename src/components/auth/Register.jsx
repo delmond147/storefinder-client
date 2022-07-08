66d3ed7aa35aa47841a14051199a009c9c469d6e
delmond147 <delmondbongha147@gmail.com>
2022-07-08 18:13:01 +0100

    Updated Commit

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,29 +1,29 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { Form,  Alert } from "react-bootstrap";
-import { useUserAuth } from "../../context/UserAuthContext";
+import React, { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { Form,  Alert } from "react-bootstrap"
+import { useUserAuth } from "../../context/UserAuthContext"
 
 const Register = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const { signUp } = useUserAuth();
-    const navigate = useNavigate();
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+    const { signUp } = useUserAuth()
+    const navigate = useNavigate()
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
-        setError("");
+        e.preventDefault()
+        setError("")
         try {
-        await signUp(email, password);
-        navigate("/login");
+        await signUp(email, password)
+        navigate("/")
         } catch (err) {
-        setError(err.message);
+        setError(err.message)
         }
-    };
+    }
 
     return (
         <>
-            <div className="container col-lg-4 col-md-6 col-sm-7 pt-2 card">
+            <div className="container bg-light col-lg-4 col-md-6 col-sm-7 pt-2 card">
                 <div className="d-flex justify-content-center align-items-center">
                     <img src="images/avatar-150.png" className="rounded-circle" alt="" />
                 </div>
@@ -53,7 +53,7 @@ const Register = () => {
 
                 <div class="col-12 mt-4 d-flex justify-content-center align-items-center">
                     <button type="submit" class="btn btn-primary px-5 py-3 ">
-                    Login
+                        Register
                     </button>
                 </div>
                 </Form>
--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import { getAuth } from 'firebase/auth'
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhIUIWzzhkfojCvLdoBDxkEnPJMFAOlr4",
-  authDomain: "store-finder-36111.firebaseapp.com",
-  projectId: "store-finder-36111",
-  storageBucket: "store-finder-36111.appspot.com",
-  messagingSenderId: "719436321638",
-  appId: "1:719436321638:web:69fe8f866101420a99eea7",
-  measurementId: "G-Z0Q6MKX050",
-  databaseURL: "https://store-finder-36111-default-rtdb.firebaseio.com/"
+  apiKey: "AIzaSyANFeok45D5NBSpw7eK2AddUbrPYHoc_P4",
+  authDomain: "react-authentication-d8bf6.firebaseapp.com",
+  projectId: "react-authentication-d8bf6",
+  storageBucket: "react-authentication-d8bf6.appspot.com",
+  messagingSenderId: "71724455169",
+  appId: "1:71724455169:web:44f4912722022b9d6ff700",
+  measurementId: "G-XK5VT7X4ZV",
+  databaseURL: "https://react-authentication-d8bf6-default-rtdb.firebaseio.com/"
 };
 
 // Initialize Firebase
